4f974b6837b0aef9a3efe0227c772e0293e4e83c
Dillion <75215151+DillionB@users.noreply.github.com>
2022-12-30 09:15:33 -0700

    Updated Backend
    Expanded API functionality for inventory managment

---
+++ Kisosk_Manager_Backend/Models/locationModel.cs
@@ -0,0 +1,25 @@
+﻿namespace KioskManager_Backend.Models
+{
+    public class locationModel
+    {
+        public int Id { get; set; }
+        public string TempName { get; set; }
+        public string TempDesc { get; set; }
+        public string FileUploadName { get; set; }
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+        public string TempCode { get; set; }
+        public string LeaseStart { get; set; }
+        public string LeaseEnd { get; set; }
+        public decimal FxF { get; set; }
+        public decimal FxT { get; set; }
+        public decimal FxFT { get; set; }
+        public decimal TxT { get; set; }
+        public decimal TxFT { get; set; }
+        public decimal TxTW { get; set; }
+        public decimal TxTWF { get; set; }
+        public decimal TxTH { get; set; }
+        public decimal Car { get; set; }
+        public string Insurance { get; set; }
+    }
+}
--- Kisosk_Manager_Backend/Models/ticketModel.cs
+++ Kisosk_Manager_Backend/Models/ticketModel.cs
@@ -3,9 +3,11 @@
     public class ticketModel
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
        
+        public string Title { get; set; }
+        public string Owner { get; set; }
+        public string Description { get; set; }
 
+        public string Type { get; set; }
     }
 }
