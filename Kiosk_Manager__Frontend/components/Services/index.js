27a3953ffbef68d9ad9a6d1aec0d47b200673bad
Dillion <75215151+DillionB@users.noreply.github.com>
2022-12-30 09:10:25 -0700

    Updated Frontend
    Added inventory managments functionality and optimised map map population

--- Kiosk_Manager__Frontend/components/Services/index.js
+++ Kiosk_Manager__Frontend/components/Services/index.js
@@ -21,7 +21,7 @@ const Services = () => {
           <ServicesIcon src={Icon1} />
           <ServicesH2>Reduce Expenses</ServicesH2>
           <ServicesP>
-            We help reduce your fees and increase your overall revenue.
+            We help reduce your overhead and increase overall revenue.
           </ServicesP>
         </ServicesCard>
         <ServicesCard>
@@ -35,7 +35,7 @@ const Services = () => {
           <ServicesIcon src={Icon3} />
           <ServicesH2>Premium Benefits</ServicesH2>
           <ServicesP>
-            Unlock our special membership card that returns 5% cash back.
+            Unlock our special membership card that awards you with a discount on future upgrades.
           </ServicesP>
         </ServicesCard>
       </ServicesWrapper>
